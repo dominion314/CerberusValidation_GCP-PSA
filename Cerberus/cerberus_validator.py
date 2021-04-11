93b444a22ab1de2431d4df7fc6e0ba9c29edfd4b
Dominick Hernandez <dominick.hernandez@kohls.com>
2021-04-11 12:21:10 -0500

    Cerberus working commit

--- Cerberus/cerberus_validator.py
+++ Cerberus/cerberus_validator.py
@@ -2,19 +2,21 @@
 from cerberus import Validator
 import yaml
 
-with open('processed_params.yml') as f:
+with open('PSA_PR_Format.yml') as f:
     DATA = yaml.load(f, Loader=yaml.FullLoader)
 
 with open('PSA_Schema.yaml') as f:
     SCHEMA = yaml.load(f, Loader=yaml.FullLoader)
 
-print('Hellow World')
 V = Validator()
 V.allow_unknown = True
 
 if V.validate(DATA, SCHEMA):
     print('Pass')
+    print(V.document)
 else:
     print('Fail')
     ERR = V.errors
     print(ERR)
+
+#1. 
